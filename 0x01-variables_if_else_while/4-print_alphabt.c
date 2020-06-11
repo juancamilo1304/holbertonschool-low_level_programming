be4b135ae9d7375372a500c6c1acb1ba292f2dce
juancamilo1304 <camiloo1304@hotmail.com>
2020-06-11 23:50:58 +0000

    hey! this is the correct print alphabet 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-  char low, e, q;
+	char low, e, q;
 
-  e = 'e';
-  q = 'q';
+	e = 'e';
+	q = 'q';
 
-  for (low = 'a'; low <= 'z'; low++)
-    {
-      if (low != e && low != q)
-	putchar(low);
-    }
-  printf("\n");
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	printf("\n");
 
-  return (0);
- }
+	return (0);
+}
